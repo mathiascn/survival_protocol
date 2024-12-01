7b12362c699140ec56ac216c2248e2d01ce87832
mathiaas@live.no <mathiaas@live.no>
2024-12-01 12:49:42 +0100

    add config singleton

---
+++ scripts/Config.gd
@@ -0,0 +1,16 @@
+extends Node
+
+static var version_major: int = 0
+static var version_minor: int = 1
+static var version_patch: int = 1
+
+static var server_version_major: int = 0
+static var server_version_minor: int = 1
+static var server_version_patch: int = 0
+
+static var game_name: String = "Survival Protocol"
+static var expected_server_type: String = "survival_protocol"
+
+static var host: String = "localhost"
+static var port: int = 8080
+static var connection_timeout: int = 5
